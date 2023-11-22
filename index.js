deff050cb7f58635b52bbd69c83a6a3796eec84b
Laurynasjoksas88 <144778600+Laurynasjoksas88@users.noreply.github.com>
2023-11-23 00:40:17 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,115 @@
+"use strict";
+// pirma uzduotis
+
+// function hello() {
+//     console.log("Sveiki, Martynai");
+// }
+
+// function greeting() {
+//     let name = 'Reksas'
+//     hello();
+//     console.log("Augintinis, " + name + "!");
+// }
+
+
+// hello()
+// hello()
+// greeting();
+// greeting();
+
+// antra uzduotis 
+
+// function priimaSkaicius(pirmasSkaicius, antrasSkaicius) {
+
+//     if (pirmasSkaicius > antrasSkaicius) {
+//         console.log('Pirmas skaičius didesnis');
+//     }
+//     if (pirmasSkaicius < antrasSkaicius) {
+//         console.log('Antrasis skaičius didesnis');
+//     }
+//     if (pirmasSkaicius == antrasSkaicius) {
+//         console.log('Skaičiai lygūs' +  (pirmasSkaicius + antrasSkaicius));
+//     }
+
+
+// }
+
+// priimaSkaicius(5, 5)
+
+// trecia uzduotis 
+
+// function fizzandBazz() {
+//    let userInput = prompt("Įveskie fizz arba bazz:");
+
+//    if (userInput === 'fizz') {
+//     console.log('bazz')
+//    } else if (userInput === 'bazz') {
+//     console.log('fizz')
+//    } else {
+//     console.log('Please provide fizz or bazz')
+//    }
+
+// }
+// fizzandBazz()
+
+// ketvirta uzduotis 
+
+// function patikrintiArTrikampis() {
+//     let akrastine = prompt('Iveskite skaiciu a krastinei')
+//     let bkrastine = prompt('Iveskite skaiciu b krastinei')
+//     let ckrastine = prompt('Iveskite skaiciu c krastinei')
+
+//     if (akrastine + bkrastine == ckrastine) {
+//         console.log('Yra trikampis');
+//     } else {
+//         console.log('Nera trikampis');
+//     }
+// }
+// patikrintiArTrikampis()
+
+// penkta uzduotis
+
+// function skaiciuojaKvadrata() {
+
+//     let akrastine = prompt('Iveskite skaiciu a krastine')
+
+//     let plotas = akrastine * akrastine
+// console.log(plotas)
+//     return plotas
+
+// }
+//   function skaiciuojaTrikampi() {
+//     let akrastine = prompt('Iveskite skaiciu a krastinei')
+//     let bkrastine = prompt('Iveskite skaiciu b krastinei')
+    
+//     let plotas = (akrastine * bkrastine) /2
+//     console.log(plotas)
+//     return plotas
+//   }
+//   function skaiciuojaFigurosPlota(figura) {
+//     if (figura === 'kvadratas' ) {
+//         return skaiciuojaKvadrata()
+//     } else if (figura === 'trikampis' ) {
+//         return skaiciuojaTrikampi()
+//     } else {
+//         alert('Netinkamas figuros pavadinimas')
+//     }
+
+//   }
+
+//   let inputas = prompt('Iveskite figuros pavadinima').toLowerCase()
+
+//   skaiciuojaFigurosPlota(inputas)
+  
+
+
+
+
+
+
+
+
+
+
+
+
